5c01eb4a93bdccf2a20fd12e33f7fdf3bca20b5a
Phạm Xuân Hoài <50172777+kitajima2910@users.noreply.github.com>
2024-05-19 17:52:26 +0700

    Show list users (#2)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.scss';
 import Header from './components/Header';
 import TableUSers from './components/TableUsers';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className='app-container'>
-      <Header/>
-      <TableUSers/>      
+      <Header />
+
+      <Container>
+        <TableUSers />
+      </Container>
     </div>
   );
 }
