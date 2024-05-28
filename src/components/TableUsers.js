b868e8e7fbde6cfade51d9dcc80b3a674569ba46
kitajima2910 <kitajima2910@gmail.com>
2024-05-28 18:46:41 +0700

    Paginate Users

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,21 +1,30 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
 const TableUSers = (props) => {
 
     const [listUsers, setListUsers] = useState([])
+    const [totalUsers, setTotalUsers] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
 
     useEffect(() => {
         // call apis
-        getUsers()
+        getUsers(1)
     }, [])
 
-    const getUsers = async () => {
-        let res = await fetchAllUser()
+    const getUsers = async (page) => {
+        let res = await fetchAllUser(page)
         if (res && res.data) {
+            setTotalUsers(res.total)
             setListUsers(res.data)
+            setTotalPages(res.total_pages)
         }
+    }
+
+    const handlePageClick = (event) => {
+        getUsers(+event.selected + 1)
     }
 
     console.log(listUsers);
@@ -45,6 +54,26 @@ const TableUSers = (props) => {
                     }
                 </tbody>
             </Table>
+            <ReactPaginate
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakLabel="..."
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+                renderOnZeroPageCount={null}
+            />
         </>
     )
 }
