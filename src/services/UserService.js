e005a8cbe803299a911324611e6efa017f1dc773
Phạm Xuân Hoài <50172777+kitajima2910@users.noreply.github.com>
2024-06-01 12:18:03 +0700

    Modal Confirm Delete (#8)

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -12,4 +12,8 @@ const putUpdateUser = (name, job, id) => {
     return axios.put(`/api/users/${id}`, { name, job })
 }
 
-export { fetchAllUser, postCreateUser, putUpdateUser }
+const deleteUser = (id) => {
+    return axios.delete(`/api/users/${id}`)
+}
+
+export { fetchAllUser, postCreateUser, putUpdateUser, deleteUser }
